19439b9a08c33b970864ac1cb7a455b8b6efd1f9
Douglas Baisso <85198572+dbaisso@users.noreply.github.com>
2022-04-07 17:36:41 -0300

    I´m uploding Readme Projetos_estudo001.py and projeto_estudo001.py

---
+++ Readme Projeto_estudo001.md
@@ -0,0 +1,5 @@
+###Readme - projetos_estudo001.py###
+
+**No arquivo projetos_estudo001.py começo utilizando algumas funções e variáveis.
+**São exercícios propostos do Prof° Gustavo Gunabara do canal Curso em vídeo.
+**Aula e exercícios da aula 01 até 07.
---
+++ projeto_estudo001.py
@@ -0,0 +1,53 @@
+# print('Olá Mundo!')
+# print('Esse será meu primeiro projeto e preciso saber algumas informações suas, pode me ajudar?')
+# nome = input('Qual seu nome?')
+# idade = input('Agora, qual sua idade?')
+# print('Muito bem',nome,', sua idade é',idade,'anos. Correto?')
+# print('Também aprendi a fazer contas, mas tomando cuidado em como codificar para que seja mostrada o resultado da
+# função e não na sintáxe texto. Também devemos usaros tipos primitivos para identificar e converter o resultado  como
+
+# int, float, bool ou str.')
+# numero1 = int (input('Digite seu primeiro número para fazermos os exemplos: '))
+# numero2 = int (input('Pode colocar o segundo agora: '))
+# soma = numero1 + numero2
+# print('A soma de {} com {} será {} correto?'.format(numero1, numero2, soma))
+
+# print('Existem várias maneiras de verificar o que o usuário está digitando e traçar "regras" para saber o tipo delas.'
+# 'A seguir são aguns exemplos de como identifica las.')
+# n0 = input('Digite algo: ')
+# print(type(n0))
+# print(n0.isalnum())
+# print('Na linha acima, foi identificado o tipo primitivo String e alfa-númerico.')
+
+# n1 = int(input('Digite numeros: '))
+# print(type(n1))
+# print(n1)
+# print('Na linha acima, foi identificado o tipo primitivo Integer.')
+
+# n2 = bool(input('Digite algo: '))
+# print(type(n2))
+# print(n2)
+# print('Na linha acima, foi identificado o tipo primitivo Boolean. Se digitar algo receberá True do contrário False.')
+
+# n3 = float(input('Digite algum número decimal: '))
+# print(type(n3))
+# print('Na linha acima, foi identificado o tipo primitivo Float.')
+# print('Conteúdo das aulas curso em vídeo 1 a 6')
+
+# teste para saber o tipo de caractere que está pode ser convertido ou
+# print('Pode ser usando letras, números, maiúsculo, minúsculo ou qualquer outro caractere!')
+# aleatorio = input('Digite algo: ')
+# print('Você digitou: {}. É numérico?'.format(aleatorio),aleatorio.isnumeric())
+# print('Você digitou: {}. É numérico decimal? '.format(aleatorio),aleatorio.isdecimal())
+# print('Você digitou: {}. É alfanumérico? '.format(aleatorio),aleatorio.isalnum())
+# print('Você digitou: {}. Está em minúsculo? '.format(aleatorio),aleatorio.islower())
+# print('Você digitou: {}. Está em maiúsculo? '.format(aleatorio),aleatorio.isupper())
+# print('Você digitou: {}. Só tem espaços? '.format(aleatorio),aleatorio.isspace())
+# print('Está capitalizado? (Maiusculas e Minusculas)'.format(aleatorio),aleatorio.istitle())
+
+# ordem de precedência para funções aritiméticas:
+# 1°() o que está entre parênteses e só se usar parenteses, nunca chaves ou colchetes.
+# 2°** potência
+# *,/,//,%
+# +,-
+# == simbo de igualdade aritimética no python
