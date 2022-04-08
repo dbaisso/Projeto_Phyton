0f2bb18b1ae660d0df1f6cd851567a8fa32f4995
Douglas Baisso <85198572+dbaisso@users.noreply.github.com>
2022-04-08 17:47:08 -0300

    Uploading file Readme Projeto_estudo002.md and projeto_estudo002.py study math arithmetic

---
+++ Readme Projeto_estudo002.md
@@ -0,0 +1,4 @@
+##Readme Projeto_estudo002##
+#No arquivo Projeto_estudo002 fiz uso da linguagem python para resolver exercícios de matemática aritimética.
+#Projeto para calculo da média de valores, raiz quadrada, multiplicação e também conversão de valores.
+#Uso de alguns códigos para formatar o resultado para melhor visualização como "\n" e dentro de colchetes ":.2f" formatando números decimais.
---
+++ projeto_estudo002.py
@@ -0,0 +1,31 @@
+##Criar um algoritimo que leia um número mostre o dobro o tripo e a raiz quadrada.
+#n = int(input('Digite um número: '))
+#d = n * 2
+#t = n * 3
+#r = n ** (1/2)
+#print('Você digitou {}.\nO dobro é {}, o triplo é {}.\nA raiz quadrada de {} é {:.2f}'.format(n, d, t, n, r))
+
+##Acima temos algumas dicas de formatação dentro dos texto para quebrer linhas e formatação nas casas decimais com duas
+##casas decimais.
+##Desenvolver um programa que leia dois números e e calcule a média.
+#n1 = float(input('Digite o primeiro número: '))
+#n2 = float(input('Digite o segundo número: '))
+#media = (n1 + n2)/2
+#print('A média entre {} e {} é {:.1f}'.format(n1, n2, media))
+
+##Criar um conversor de medidas em metros
+#conversor = float(input('Distância em metros:'))
+#milha = conversor / 0.000621371
+#mn = conversor / 0.000539957
+#km = conversor / 1000
+#cm = conversor / 100
+#ml = conversor / 1000
+#pl = conversor / 39.37
+#jd = conversor / 1.09361
+#print('Milhas: {:.2f} \nMilhas náuticas: {:.2f} \nQuilometros: {} '
+#      '\nCentimetro: {} \nMilímetro: {} \nPolegada: {:.2f} \nJardas: {:.2f}'.format(milha, mn, km, cm, ml, pl, jd))
+
+
+
+
+
